7d1f1e6841882b2aa6735081a67a650326fd6d34
vulenguyentruong-sys <vulenguyentruong@gmail.com>
2026-05-31 12:43:16 +0700

    Create models.py

---
+++ models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.conf import settings
+
+# Giả sử bạn đã có model Course, Lesson, Enrollment phía trên
+# ...
+
+class Question(models.Model):
+    lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE)
+    question_text = models.CharField(max_length=200)
+    grade = models.IntegerField(default=1)
+
+    def __str__(self):
+        return self.question_text
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    is_correct = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.choice_text
+
+class Submission(models.Model):
+    enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
+    choices = models.ManyToManyField(Choice)
+
+    def __str__(self):
+        return f"Submission by {self.enrollment}"
